d26e178daf88ec40e96c083bb9347936546d4555
Asaf Cohen <asafc@jfrog.com>
2026-07-27 23:19:54 +0300

    removed vulns
    removed vulns

--- app.js
+++ app.js
@@ -6,11 +6,8 @@ const fs = require('fs');
 const app = express()
 const port = 8080
 const api_key = "2VTHzn1mKZ/n9apD5P6nxsajSQh8QhmyyKvUIRoZWAHCB8lSbBm3YWx5nOdZ1zPEOaA0zIZy1eFgHgfB2HkfAdVrbQj19kagXDVe"
-// jfrog-ignore 
-const api_key2 = "PrxQm6WxUq-Eb5ujhf6K"
 const api_key3 = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYzEAMPLEKEYPrxQm6WxUq-Eb5ujhf6K"
 const api_key4 = "cmVmdGtuOjAxOjE3NzAzMTgzMTQ6MmgzSWZDTTRBdjVOdWFoS2dRblh0MEtJd2Rs"
-const api_key5 = "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"
 
 
 function parseUrl(usrUrl){
@@ -38,22 +35,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+'/views/index.html'));
 
 })
-
-app.post("/uploadFile", (req, res) => {
-  if (!req.files) {
-    return res.status(400).send("No files were uploaded.");
-  }
-
-  const file = req.files.myFile;
-  const path = __dirname + "/uploads/" + file.name;
-
-  file.mv(path, (err) => {
-    if (err) {
-      return res.status(500).send(err);
-    }
-    return res.send({ status: "success", path: path });
-  });
-});
 
 app.post("/uploadPath", (req, res) => {
   const usrUrl = req.body.myURL
